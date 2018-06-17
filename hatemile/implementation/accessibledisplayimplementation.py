bcd6010b89a15b0f94742b91c5604af30c3a3018
3837162+carlsonsantana@users.noreply.github.com <carlsonsantana@gmail.com>
2018-06-17 12:39:29 -0300

    :sparkles: Display shortcut before or after element

--- hatemile/implementation/accessibledisplayimplementation.py
+++ hatemile/implementation/accessibledisplayimplementation.py
@@ -39,6 +39,14 @@ class AccessibleDisplayImplementation(AccessibleDisplay):
     #: descriptions.
     CLASS_TEXT_SHORTCUTS = 'text-shortcuts'
 
+    #: The HTML class of content to force the screen reader show the current
+    #: state of element, before it.
+    CLASS_FORCE_READ_BEFORE = 'force-read-before'
+
+    #: The HTML class of content to force the screen reader show the current
+    #: state of element, after it.
+    CLASS_FORCE_READ_AFTER = 'force-read-after'
+
     #: The name of attribute that links the description of shortcut of element.
     DATA_ATTRIBUTE_ACCESSKEY_OF = 'data-attributeaccesskeyof'
 
@@ -66,6 +74,18 @@ class AccessibleDisplayImplementation(AccessibleDisplay):
         )
         self.attribute_accesskey_after = configure.get_parameter(
             'attribute-accesskey-after'
+        )
+        self.attribute_accesskey_prefix_before = configure.get_parameter(
+            'attribute-accesskey-prefix-before'
+        )
+        self.attribute_accesskey_suffix_before = configure.get_parameter(
+            'attribute-accesskey-suffix-before'
+        )
+        self.attribute_accesskey_prefix_after = configure.get_parameter(
+            'attribute-accesskey-prefix-after'
+        )
+        self.attribute_accesskey_suffix_after = configure.get_parameter(
+            'attribute-accesskey-suffix-after'
         )
         self.list_shortcuts_added = False
         self.list_shortcuts_before = None
@@ -258,6 +278,166 @@ class AccessibleDisplayImplementation(AccessibleDisplay):
                     container_after.append_element(self.list_shortcuts_after)
         self.list_shortcuts_added = True
 
+    def _insert(self, element, new_element, before):
+        """
+        Insert a element before or after other element.
+
+        :param element: The reference element.
+        :type element: hatemile.util.html.htmldomelement.HTMLDOMElement
+        :param new_element: The element that be inserted.
+        :type new_element: hatemile.util.html.htmldomelement.HTMLDOMElement
+        :param before: To insert the element before the other element.
+        :type before: bool
+        """
+
+        tag_name = element.get_tag_name()
+        append_tags = [
+            'BODY',
+            'A',
+            'FIGCAPTION',
+            'LI',
+            'DT',
+            'DD',
+            'LABEL',
+            'OPTION',
+            'TD',
+            'TH'
+        ]
+        controls = ['INPUT', 'SELECT', 'TEXTAREA']
+        if tag_name == 'HTML':
+            body = self.parser.find('body').first_result()
+            if body is not None:
+                self._insert(body, new_element, before)
+        elif tag_name in append_tags:
+            if before:
+                element.prepend_element(new_element)
+            else:
+                element.append_element(new_element)
+        elif tag_name in controls:
+            labels = []
+            if element.has_attribute('id'):
+                labels = self.parser.find(
+                    'label[for="'
+                    + element.get_attribute('id')
+                    + '"]'
+                ).list_results()
+            if not labels:
+                labels = self.parser.find(element).find_ancestors(
+                    'label'
+                ).list_results()
+            for label in labels:
+                self._insert(label, new_element, before)
+        elif before:
+            element.insert_before(new_element)
+        else:
+            element.insert_after(new_element)
+
+    def _force_read_simple(self, element, text_before, text_after, data_of):
+        """
+        Force the screen reader display an information of element.
+
+        :param element: The reference element.
+        :type element: hatemile.util.html.htmldomelement.HTMLDOMElement
+        :param text_before: The text content to show before the element.
+        :type text_before: str
+        :param text_after: The text content to show after the element.
+        :type text_after: str
+        :param data_of: The name of attribute that links the content with
+                        element.
+        :type data_of: str
+        """
+
+        self.id_generator.generate_id(element)
+        identifier = element.get_attribute('id')
+        selector = '[' + data_of + '="' + identifier + '"]'
+
+        reference_before = self.parser.find(
+            '.'
+            + AccessibleDisplayImplementation.CLASS_FORCE_READ_BEFORE
+            + selector
+        ).first_result()
+        reference_after = self.parser.find(
+            '.'
+            + AccessibleDisplayImplementation.CLASS_FORCE_READ_AFTER
+            + selector
+        ).first_result()
+        references = self.parser.find(selector).list_results()
+        if reference_before in references:
+            references.remove(reference_before)
+        if reference_after in references:
+            references.remove(reference_after)
+
+        if not references:
+            if text_before:
+                if reference_before is not None:
+                    reference_before.remove_node()
+
+                span = self.parser.create_element('span')
+                span.set_attribute(
+                    'class',
+                    AccessibleDisplayImplementation.CLASS_FORCE_READ_BEFORE
+                )
+                span.set_attribute(data_of, identifier)
+                span.append_text(text_before)
+                self._insert(element, span, True)
+            if text_after:
+                if reference_after is not None:
+                    reference_after.remove_node()
+
+                span = self.parser.create_element('span')
+                span.set_attribute(
+                    'class',
+                    AccessibleDisplayImplementation.CLASS_FORCE_READ_AFTER
+                )
+                span.set_attribute(data_of, identifier)
+                span.append_text(text_after)
+                self._insert(element, span, False)
+
+    def _force_read(
+        self,
+        element,
+        value,
+        text_prefix_before,
+        text_suffix_before,
+        text_prefix_after,
+        text_suffix_after,
+        data_of
+    ):
+        """
+        Force the screen reader display an information of element with prefixes
+        or suffixes.
+
+        :param element: The reference element.
+        :type element: hatemile.util.html.htmldomelement.HTMLDOMElement
+        :param value: The value to be show.
+        :type value: str
+        :param text_prefix_before: The prefix of value to show before the
+                                   element.
+        :type text_prefix_before: str
+        :param text_suffix_before: The suffix of value to show before the
+                                   element.
+        :type text_suffix_before: str
+        :param text_prefix_after: The prefix of value to show after the
+                                  element.
+        :type text_prefix_after: str
+        :param text_suffix_after: The suffix of value to show after the
+                                  element.
+        :type text_suffix_after: str
+        :param data_of: The name of attribute that links the content with
+                        element.
+        :type data_of: str
+        """
+
+        if (text_prefix_before) or (text_suffix_before):
+            text_before = text_prefix_before + value + text_suffix_before
+        else:
+            text_before = ''
+        if (text_prefix_after) or (text_suffix_after):
+            text_after = text_prefix_after + value + text_suffix_after
+        else:
+            text_after = ''
+        self._force_read_simple(element, text_before, text_after, data_of)
+
     def display_shortcut(self, element):
         data_attribute_accesskey_of = (
             AccessibleDisplayImplementation.DATA_ATTRIBUTE_ACCESSKEY_OF
@@ -275,15 +455,20 @@ class AccessibleDisplayImplementation(AccessibleDisplay):
                 element.get_attribute('accesskey').upper()
             )
             for key in keys:
+                shortcut = self.shortcut_prefix + ' + ' + key
+                self._force_read(
+                    element,
+                    shortcut,
+                    self.attribute_accesskey_prefix_before,
+                    self.attribute_accesskey_suffix_before,
+                    self.attribute_accesskey_prefix_after,
+                    self.attribute_accesskey_suffix_after,
+                    data_attribute_accesskey_of
+                )
+
                 item = self.parser.create_element('li')
                 item.set_attribute(data_attribute_accesskey_of, key)
-                item.append_text(
-                    self.shortcut_prefix
-                    + ' + '
-                    + key
-                    + ': '
-                    + description
-                )
+                item.append_text(shortcut + ': ' + description)
 
                 selector = (
                     '['
